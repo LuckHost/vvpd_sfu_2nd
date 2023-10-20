663e62337edb8044520aeca308085b19351c7724
LuckSeeker <sh76448@gmail.com>
2023-10-20 12:24:41 +0900

    Random array added

--- second_project.py
+++ second_project.py
@@ -1,7 +1,12 @@
 import numpy as np
 
+def get_random_array(rows = 1, columns = 1):
+    arr = np.random.randint(-10, 10, size=(rows, columns))
+    return arr
+
 def main():
-    print("Hello, world!")
+    """the main function"""
+    print(get_random_array(2, 4))
 
 if __name__ == "__main__":
     main()
